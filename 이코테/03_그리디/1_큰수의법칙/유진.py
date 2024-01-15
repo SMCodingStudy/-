391f07c7489f13ca4d9fa41b563728409f56e732
Youjiiin <yujinjang24@gmail.com>
2024-01-15 20:59:37 +0900

    파일 병합 및 이름 변경

--- 이코테/03_그리디/1_큰수의법칙/유진.py
+++
@@ -1,9 +0,0 @@
-n, m, k = map(int, input().split()) # 배열의 크기 n, 숫자가 더해지는 횟수 m, 초과 불가능 횟수 k
-arr = list(map(int, input().split()))
-arr.sort() # 오름차순 / 내림차순 차이가 있을까
-sum = 0
-a = m // k 
-b = m % k
-
-sum = arr[n - 1] * k * a + arr[n - 2] * b
-print(sum)

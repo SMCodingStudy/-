c78d2d6bf474afc748c31b5855a12711a0a6dad4
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2024-01-16 17:52:50 +0900

    미로 탈출_준호
    참고

---
+++ 이코테/05 DFS_BFS/미로_탈출/준호.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class Node {
+    private int x;
+    private int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+public class 준호 {
+
+    public static int n, m;
+    public static int[][] graph = new int[201][201];
+
+    public static int dx[] = {-1, 1, 0, 0};
+    public static int dy[] = {0, 0, -1, 1};
+
+    public static int bfs(int x, int y) {
+        Queue<Node> q = new LinkedList<>();
+//        해당 큐 맨 뒤에 값을 삽입하는데 add 와 달리 값 추가 실패시 false를 반환
+        q.offer(new Node(x, y));
+        while (!q.isEmpty()) {
+//            큐 맨 앞에 있는 값 반환 후 삭제, remove와 달리 큐가 비어있을 경우 null 반환
+            Node node = q.poll();
+            x = node.getX();
+            y = node.getY();
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+                if (graph[nx][ny] == 0) continue;
+
+                if (graph[nx][ny] == 1) {
+                    graph[nx][ny] = graph[x][y] + 1;
+                    q.offer(new Node(nx, ny));
+                }
+            }
+        }
+        return graph[n - 1][m - 1];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        scanner.nextLine(); // 버퍼 지우기
+
+        // 2차원 리스트의 맵 정보 입력 받기
+        for (int i = 0; i < n; i++) {
+            String str = scanner.nextLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        // BFS를 수행한 결과 출력
+        System.out.println(bfs(0, 0));
+    }
+}

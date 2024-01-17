4cefd815db184c7ff81366c9fe47d28906a7a09d
Youjiiin <yujinjang24@gmail.com>
2024-01-17 13:17:30 +0900

    DFS/BFS

--- 이코테/05 DFS_BFS/미로_탈출/유진.py
+++ 이코테/05 DFS_BFS/미로_탈출/유진.py
@@ -1,42 +1,29 @@
-import sys
 from collections import deque
 
 n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, sys.stdin.readline().split())))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
 
-    while queue: # 큐가 빌 때까지 반복
+    while queue:
         x, y = queue.popleft()
-
-        # 현재위치에서 4방향 확인
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            # 지형 벗어나면 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            # 괴물있는 곳 무시
             if graph[nx][ny] == 0:
                 continue
-            # 갈 수 있는 공간일 시
+
             if graph[nx][ny] == 1:
-                # 갈 수 있는 공간이면 값+1
                 graph[nx][ny] = graph[x][y] + 1
-                # 큐에 이동할 곳 넣어두기
                 queue.append((nx, ny))
-    # 그래프의 마지막 값 리턴
+
     return graph[n - 1][m - 1]
-
 print(bfs(0, 0))
-
-# 몰라서 봤음
--- 이코테/05 DFS_BFS/음료수_얼려먹기/유진.py
+++ 이코테/05 DFS_BFS/음료수_얼려먹기/유진.py
@@ -6,23 +6,23 @@ graph = []
 for _ in range(n):
     graph.append(list(map(int, sys.stdin.readline().split())))
 
-def count_ice (x, y):
-    if x < 0 or y < 0 or x > n or y > m:
+def dfs(x, y):
+    if x < 0 or y < 0 or x >= n or y >= m:
         return False
-
+    
     if graph[x][y] == 0:
         graph[x][y] = 1
-        count_ice(x + 1, y)
-        count_ice(x, y + 1)
-        count_ice(x - 1, y)
-        count_ice(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x - 1, y)
+        dfs(x, y + 1)
+        dfs(x, y - 1)
         return True
     return False
 
 ice = 0
 for i in range(n):
     for j in range(m):
-        if count_ice(n, m) == True:
+        if dfs(n, m) == True:
             ice += 1
 print(ice)
 
